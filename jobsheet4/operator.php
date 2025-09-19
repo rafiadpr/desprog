adc7c9a3a78365a4e0e3630ce7a9aeccff4f074e
rafiadpr <work.rafiadrianp@gmail.com>
2025-09-19 23:37:14 +0700

    [add] (file:jobsheet4_prak3) : completed jobsheet 4 up to all praktikum 3

--- jobsheet4/operator.php
+++ jobsheet4/operator.php
@@ -30,4 +30,47 @@ echo "Hasil Lebih Kecil : " . $hasilLebihKecil . "<br>";
 echo "Hasil Lebih Besar : " . $hasilLebihBesar . "<br>";
 echo "Hasil Lebih Kecil Sama : " . $hasilLebihKecilSama . "<br>";
 echo "Hasil Lebih Besar Sama : " . $hasilLebihBesarSama . "<br>";
+echo "<br>";
+
+$hasilAnd = $a && $b;
+$hasilOr = $a || $b;
+$hasilNotA = !$a;
+$hasilNotB = !$b;
+
+echo "Hasil And : " . $hasilAnd . "<br>";
+echo "Hasil Or : " . $hasilOr . "<br>";
+echo "Hasil Not A : " . $hasilNotA . "<br>";
+echo "Hasil Not B : " . $hasilNotB . "<br>";
+echo "<br>";
+
+$hasilTambahSamaDengan = $a += $b;
+$hasilKurangSamaDengan = $a -= $b;
+$hasilKaliSamaDengan = $a *= $b;
+$hasilBagiSamaDengan = $a /= $b;
+$hasilModulusSamaDengan = $a %= $b;
+
+echo "Hasil Tambah Sama Dengan : " . $hasilTambahSamaDengan . "<br>";
+echo "Hasil Kurang Sama Dengan : " . $hasilKurangSamaDengan . "<br>";
+echo "Hasil Kali Sama Dengan : " . $hasilKaliSamaDengan . "<br>";
+echo "Hasil Bagi Sama Dengan : " . $hasilBagiSamaDengan . "<br>";
+echo "Hasil Modulus Sama Dengan : " . $hasilModulusSamaDengan . "<br>";
+echo "<br>";
+
+$hasilIdentik = $a === $b;
+$hasilTidakIdentik = $a !== $b;
+
+echo "Hasil Identik : " . $hasilIdentik . "<br>";
+echo "Hasil Tidak Identik : " . $hasilTidakIdentik . "<br>";
+echo "<br>";
+
+$totalKursi = 45;
+$kursiTerisi = 28;
+
+$kursiKosong = $totalKursi - $kursiTerisi;
+$persentaseKosong = ($kursiKosong / $totalKursi) * 100;
+
+echo "Total kursi: $totalKursi <br>";
+echo "Kursi terisi: $kursiTerisi <br>";
+echo "Kursi kosong: $kursiKosong <br>";
+echo "Persentase kursi kosong: $persentaseKosong% <br>";
 ?>
